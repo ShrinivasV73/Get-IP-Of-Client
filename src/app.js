b1342029c1bec0b52788e0fe9e7b65ae29d42797
ShrinivasV73 <vishnupurikar.shree1973@gmail.com>
2024-08-08 12:05:38 +0530

    Added PORT and app.listen() method

--- src/app.js
+++ src/app.js
@@ -5,11 +5,12 @@ const requestIp = require('request-ip');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
-app.use(requestIp.mw());
+app.use(requestIp.mw());ßß
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../public/index.html'));
@@ -23,6 +24,10 @@ app.post('/api/submit', (req, res) => {
 	console.log(`Name: ${name}, Email: ${email}`);
 
 	res.json({ ip: clientIp });
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = app;
