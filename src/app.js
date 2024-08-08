f62eb796482e65473f7e9fcd06167b16bdad8efa
ShrinivasV73 <vishnupurikar.shree1973@gmail.com>
2024-08-08 11:44:00 +0530

    Updaed app.js file: used request-ip

--- src/app.js
+++ src/app.js
@@ -1,33 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const requestIp = require('request-ip');
 const path = require('path');
 
 const app = express();
-const port = 5500;
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('public'));
+app.use(requestIp.mw());
 
-// Serve the index.html file on the root route
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.post('/submit', (req, res) => {
-	const clientIp =
-		req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+app.post('/api/submit', (req, res) => {
+	const clientIp = req.clientIp;
 	const { name, email } = req.body;
 
 	console.log(`Received form submission from IP: ${clientIp}`);
 	console.log(`Name: ${name}, Email: ${email}`);
 
-	// Here you can save the data to a database if needed
-
 	res.json({ ip: clientIp });
 });
 
-app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
